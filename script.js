210fdc11f8cc27c3d22fe5ce28378bf85ad60fd4
Lê Khánh <88280976+lekhanh240895@users.noreply.github.com>
2024-05-08 15:10:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ const MOBILE_NET_INPUT_WIDTH = 224;
 const MOBILE_NET_INPUT_HEIGHT = 224;
 const STOP_DATA_GATHER = -1;
 const CLASS_NAMES = [];
+let mobilenet = undefined;
+let gatherDataState = STOP_DATA_GATHER;
+let videoPlaying = false;
+let trainingDataInputs = [];
+let trainingDataOutputs = [];
+let examplesCount = [];
+let predict = false;
+
 
 ENABLE_CAM_BUTTON.addEventListener("click", enableCam);
 TRAIN_BUTTON.addEventListener("click", trainAndPredict);
@@ -33,58 +41,66 @@ function enableCam() {
         ENABLE_CAM_BUTTON.classList.add("removed");
       });
     });
-  } else {
-    console.warn("getUserMedia() is not supported by your browser");
+  } else { console.warn("getUserMedia() is not supported by your browser");
+  }
+}
+
+async function trainAndPredict() {
+  predict = false;
+  tf.util.shuffleCombo(trainingDataInputs, trainingDataOutputs);
+  let outputsAsTensor = tf.tensor1d(trainingDataOutputs, 'int32');
+  let oneHotOutputs = tf.oneHot(outputsAsTensor, CLASS_NAMES.length);
+  let inputsAsTensor = tf.stack(trainingDataInputs);
+  
+  let results = await model.fit(inputsAsTensor, oneHotOutputs, {shuffle: true, batchSize: 5, epochs: 10, 
+      callbacks: {onEpochEnd: logProgress} });
+  
+  outputsAsTensor.dispose();
+  oneHotOutputs.dispose();
+  inputsAsTensor.dispose();
+  predict = true;
+  predictLoop();
+}
+
+function logProgress(epoch, logs) {
+  console.log('Data for epoch ' + epoch, logs);
+}
+
+function predictLoop() {
+  if (predict) {
+    tf.tidy(function() {
+      let videoFrameAsTensor = tf.browser.fromPixels(VIDEO).div(255);
+      let resizedTensorFrame = tf.image.resizeBilinear(videoFrameAsTensor,[MOBILE_NET_INPUT_HEIGHT, 
+          MOBILE_NET_INPUT_WIDTH], true);
+
+      let imageFeatures = mobilenet.predict(resizedTensorFrame.expandDims());
+      let prediction = model.predict(imageFeatures).squeeze();
+      let highestIndex = prediction.argMax().arraySync();
+      let predictionArray = prediction.arraySync();
+
+      STATUS.innerText = 'Prediction: ' + CLASS_NAMES[highestIndex] + ' with ' + Math.floor(predictionArray[highestIndex] * 100) + '% confidence';
+    });
+
+    window.requestAnimationFrame(predictLoop);
   }
 }
 
 /**
- * Handle Data Gather for button mouseup/mousedown.
+ * Purge data and start over. Note this does not dispose of the loaded 
+ * MobileNet model and MLP head tensors as you will need to reuse 
+ * them to train a new model.
  **/
-function gatherDataForClass() {
-  let classNumber = parseInt(this.getAttribute("data-1hot"));
-  gatherDataState =
-    gatherDataState === STOP_DATA_GATHER ? classNumber : STOP_DATA_GATHER;
-  dataGatherLoop();
-}
-
-function dataGatherLoop() {
-  if (videoPlaying && gatherDataState !== STOP_DATA_GATHER) {
-    let imageFeatures = tf.tidy(function () {
-      let videoFrameAsTensor = tf.browser.fromPixels(VIDEO);
-      let resizedTensorFrame = tf.image.resizeBilinear(
-        videoFrameAsTensor,
-        [MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH],
-        true
-      );
-      let normalizedTensorFrame = resizedTensorFrame.div(255);
-      return mobilenet.predict(normalizedTensorFrame.expandDims()).squeeze();
-    });
-
-    trainingDataInputs.push(imageFeatures);
-    trainingDataOutputs.push(gatherDataState);
-
-    // Intialize array index element if currently undefined.
-    if (examplesCount[gatherDataState] === undefined) {
-      examplesCount[gatherDataState] = 0;
-    }
-    examplesCount[gatherDataState]++;
-
-    STATUS.innerText = "";
-    for (let n = 0; n < CLASS_NAMES.length; n++) {
-      STATUS.innerText +=
-        CLASS_NAMES[n] + " data count: " + examplesCount[n] + ". ";
-    }
-    window.requestAnimationFrame(dataGatherLoop);
-  }
-}
-
-function trainAndPredict() {
-  // TODO: Fill this out later in the codelab!
-}
-
 function reset() {
-  // TODO: Fill this out later in the codelab!
+  predict = false;
+  examplesCount.length = 0;
+  for (let i = 0; i < trainingDataInputs.length; i++) {
+    trainingDataInputs[i].dispose();
+  }
+  trainingDataInputs.length = 0;
+  trainingDataOutputs.length = 0;
+  STATUS.innerText = 'No data collected';
+  
+  console.log('Tensors in memory: ' + tf.memory().numTensors())
 }
 
 let dataCollectorButtons = document.querySelectorAll("button.dataCollector");
@@ -94,18 +110,6 @@ for (let i = 0; i < dataCollectorButtons.length; i++) {
   // Populate the human readable names for classes.
   CLASS_NAMES.push(dataCollectorButtons[i].getAttribute("data-name"));
 }
-
-function gatherDataForClass() {
-  // TODO: Fill this out later in the codelab!
-}
-
-let mobilenet = undefined;
-let gatherDataState = STOP_DATA_GATHER;
-let videoPlaying = false;
-let trainingDataInputs = [];
-let trainingDataOutputs = [];
-let examplesCount = [];
-let predict = false;
 
 /**
  * Loads the MobileNet model and warms it up so ready for use.
@@ -150,3 +154,44 @@ model.compile({
   // As this is a classification problem you can record accuracy in the logs too!
   metrics: ["accuracy"],
 });
+
+/**
+ * Handle Data Gather for button mouseup/mousedown.
+ **/
+function gatherDataForClass() {
+  let classNumber = parseInt(this.getAttribute("data-1hot"));
+  gatherDataState =
+    gatherDataState === STOP_DATA_GATHER ? classNumber : STOP_DATA_GATHER;
+  dataGatherLoop();
+}
+
+function dataGatherLoop() {
+  if (videoPlaying && gatherDataState !== STOP_DATA_GATHER) {
+    let imageFeatures = tf.tidy(function () {
+      let videoFrameAsTensor = tf.browser.fromPixels(VIDEO);
+      let resizedTensorFrame = tf.image.resizeBilinear(
+        videoFrameAsTensor,
+        [MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH],
+        true
+      );
+      let normalizedTensorFrame = resizedTensorFrame.div(255);
+      return mobilenet.predict(normalizedTensorFrame.expandDims()).squeeze();
+    });
+
+    trainingDataInputs.push(imageFeatures);
+    trainingDataOutputs.push(gatherDataState);
+
+    // Intialize array index element if currently undefined.
+    if (examplesCount[gatherDataState] === undefined) {
+      examplesCount[gatherDataState] = 0;
+    }
+    examplesCount[gatherDataState]++;
+
+    STATUS.innerText = "";
+    for (let n = 0; n < CLASS_NAMES.length; n++) {
+      STATUS.innerText +=
+        CLASS_NAMES[n] + " data count: " + examplesCount[n] + ". ";
+    }
+    window.requestAnimationFrame(dataGatherLoop);
+  }
+}
